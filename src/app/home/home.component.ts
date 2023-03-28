09f45bb61bb495d2c7e37d3b503f8421021ae2ba
silva-eduardrs <silva.eduardrs@gmail.com>
2023-03-27 22:19:18 -0300

    comunicação entre componentes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { CardComponent } from '../shared/components/card/card.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild(CardComponent) card: CardComponent;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('ngOnInit');
+    console.log('card: ', this.card);
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    console.log('card: ', this.card);
+    //this.card.clickCard();
+  }
+
+  clickCard(event = 'default') {
+    console.log(event);
+    window.alert('clickCard');
+  }
 }
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -3,17 +3,19 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
   @Input() tituloCard: string;
   @Input() icone: string;
   @Output() cardSelecionado: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {}
 
-  clickCard() {}
-
+  clickCard() {
+    console.log('clickCard');
+    this.cardSelecionado.emit(this.tituloCard);
+  }
 }
